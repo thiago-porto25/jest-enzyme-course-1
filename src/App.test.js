d9dde0b6da0cbdc72633ddbd225b5cb648969e14
Thiago Porto <thiago.porto@d3.do>
2022-02-13 09:17:29 -0300

    start work on counter app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe('testing counter', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  afterEach(() => {
+    wrapper = undefined;
+  });
+
+  test('renders without error', () => {
+    const node = wrapper.find('[data-test="component-app"]');
+
+    expect(node.length).toBe(1);
+  });
+
+  test('renders increment button', () => {
+    const button = wrapper.find('[data-test="component-button"]');
+  });
+
+  test('renders counter display', () => {
+    const counterDisplay = wrapper.find(
+      '[data-test="component-counter-display"]'
+    );
+  });
+
+  test('counter display starts at 0', () => {
+    const counterDisplay = wrapper.find(
+      '[data-test="component-counter-display"]'
+    );
+  });
+
+  test('clicking the button increments counter display', () => {
+    const button = wrapper.find('[data-test="component-button"]');
+    const counterDisplay = wrapper.find(
+      '[data-test="component-counter-display"]'
+    );
+  });
 });
